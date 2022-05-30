1241490e3ddbe2941ae7e39a508f528b0db661a9
Sebion06 <sebastianmoticica@gmail.com>
2022-05-30 22:59:15 +0300

    remove comments

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
-# Run this line in Colab to install the package if it is
-# not already installed.
+
 from PIL import Image
 from IPython.display import display
 import torch as th
@@ -11,14 +10,9 @@ from glide_text2im.model_creation import (
     model_and_diffusion_defaults_upsampler
 )
 
-# This notebook supports both CPU and GPU.
-# On CPU, generating one sample may take on the order of 20 minutes.
-# On a GPU, it should be under a minute.
-
 has_cuda = th.cuda.is_available()
 device = th.device('cpu' if not has_cuda else 'cuda')
 
-# Create base model.
 def create_base_model(diffusion_steps):
     options = model_and_diffusion_defaults()
     options['use_fp16'] = has_cuda
@@ -33,7 +27,6 @@ def create_base_model(diffusion_steps):
     return model, options, diffusion;
 
 
-# Create upsampler model.
 def create_upsampler_model(diffusion_steps):
     options_up = model_and_diffusion_defaults_upsampler()
     options_up['use_fp16'] = has_cuda
@@ -48,7 +41,6 @@ def create_upsampler_model(diffusion_steps):
     return model_up, options_up, diffusion_up;
 
 
-# Create a classifier-free guidance sampling function
 def model_fn(x_t, ts, **kwargs):
     half = x_t[: len(x_t) // 2]
     combined = th.cat([half, half], dim=0)
@@ -75,23 +67,16 @@ def save_images(prompt, batch: th.Tensor):
     display(image)
 
 
-##############################
-# Sample from the base model #
-##############################
 def create_base_model_kwargs(base_options, prompt, batch_size=1):
-# Create the text tokens to feed to the model.
     tokens = base_model.tokenizer.encode(prompt)
     tokens, mask = base_model.tokenizer.padded_tokens_and_mask(
         tokens, base_options['text_ctx']
     )
-
-    # Create the classifier-free guidance tokens (empty)
    
     uncond_tokens, uncond_mask = base_model.tokenizer.padded_tokens_and_mask(
         [], base_options['text_ctx']
     )
 
-    # Pack the tokens together into model kwargs.
     model_kwargs = dict(
         tokens=th.tensor(
             [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
@@ -103,7 +88,6 @@ def create_base_model_kwargs(base_options, prompt, batch_size=1):
         ),
     )
     return model_kwargs
-# Sample from the base model.
 
 
 def get_base_sample(base_model_kwargs, base_options, base_diffusion, batch_size=1, full_batch_size=2):
@@ -119,21 +103,13 @@ def get_base_sample(base_model_kwargs, base_options, base_diffusion, batch_size=
     return samples
 
 
-##############################
-# Upsample the 64x64 samples #
-##############################
-
 def create_upsampler_model_kwargs(sample, upsample_model, upsample_options, prompt, batch_size=1):
     tokens = upsample_model.tokenizer.encode(prompt)
     tokens, mask = upsample_model.tokenizer.padded_tokens_and_mask(
         tokens, upsample_options['text_ctx']
     )
-    # Create the model conditioning dict.
     model_kwargs = dict(
-        # Low-res image to upsample.
         low_res=((sample+1)*127.5).round()/127.5 - 1,
-
-        # Text tokens
         tokens=th.tensor(
             [tokens] * batch_size, device=device
         ),
@@ -147,7 +123,6 @@ def create_upsampler_model_kwargs(sample, upsample_model, upsample_options, prom
 
 
 def get_upsampled_sample(up_model_kwargs, upsample_model, upsample_options, upsample_diffusion, batch_size, upsample_temp):
-    # Sample from the base model.
     upsample_model.del_cache()
     up_shape = (batch_size, 3, upsample_options["image_size"], upsample_options["image_size"])
     up_samples = upsample_diffusion.ddim_sample_loop(
@@ -163,7 +138,6 @@ def get_upsampled_sample(up_model_kwargs, upsample_model, upsample_options, upsa
     return up_samples
 
 
-# Sampling parameters
 def main():
     global guidance_scale
     global base_model
@@ -172,8 +146,6 @@ def main():
     guidance_scale = 3.0
     full_batch_size = batch_size * 2
 
-    # Tune this parameter to control the sharpness of 256x256 images.
-    # A value of 1.0 is sharper, but sometimes results in grainy artifacts.
     upsample_temp = 0.997
     base_diffusion_steps = '100'
     umsampler_diffusion_steps = 'fast27'
@@ -187,7 +159,6 @@ def main():
     base_sample = get_base_sample(base_model_kwargs, base_options, base_diffusion, batch_size, full_batch_size)
     base_model.del_cache()
     
-    # Show the output
     save_images(prompt, base_sample)
 
     up_model_kwargs = create_upsampler_model_kwargs(base_sample, upsample_model, upsample_options, prompt, batch_size)
@@ -195,7 +166,6 @@ def main():
     up_sample = get_upsampled_sample(up_model_kwargs, upsample_model, upsample_options, upsample_diffusion, batch_size, upsample_temp)
     upsample_model.del_cache()
 
-    # Show the output
     save_images(prompt, up_sample)
 
 if __name__ == '__main__':
